96b486d4932fea96d81b2e926abd48e6fe4d3561
Joel <esteban_joel@hotmail.com>
2023-03-29 00:06:13 -0300

    updated details cards with data from API

--- assets/data/details.js
+++ assets/data/details.js
@@ -1,31 +1,48 @@
-let events = data.events 
+let dataEvents = []
+const API_URL = "https://mindhub-xj03.onrender.com/api/amazing"
 
 const queryString = location.search 
-
 const params = new URLSearchParams(queryString)
-
 const id = params.get("id") 
-
-const eventDetailed = events.find(evento => evento._id == id) 
-
 const contenedor = document.getElementById("container-detail")
 
-tarjetas =  `<div class="card" style="width: 36rem;">
-                <img src="${eventDetailed.image}" class="card-img-top" alt="foto de ${eventDetailed.name}">
-                <div class="card-body">
-                    <h5 class="card-title fw-bolder">${eventDetailed.name}</h5>
-                    <p class="card-text">${eventDetailed.description}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Date: ${eventDetailed.date}</li>
-                    <li class="list-group-item">Category: ${eventDetailed.category}</li>
-                    <li class="list-group-item">Price: $${eventDetailed.price}</li>
-                </ul>
-                <div class="card-body">
-                    <li class="list-group-item">Place: ${eventDetailed.place}</li>
-                    <li class="list-group-item">Capacity: ${eventDetailed.capacity}</li>
-                </div>
-            </div>`
+async function getEvents() {
+    try {
+        const response = await fetch(API_URL);
+        const eventsFromAPI = await response.json();
 
-contenedor.innerHTML = tarjetas
-                    
+        for (const event of eventsFromAPI.events) {
+            dataEvents.push(event)
+        }
+        console.log(dataEvents)
+        
+        const eventDetailed = dataEvents.find(evento => evento._id == id) 
+        console.log(eventDetailed)
+
+
+        tarjetas =  `<div class="card" style="width: 36rem;">
+                        <img src="${eventDetailed.image}" class="card-img-top" alt="foto de ${eventDetailed.name}">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bolder">${eventDetailed.name}</h5>
+                            <p class="card-text">${eventDetailed.description}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Date: ${eventDetailed.date}</li>
+                            <li class="list-group-item">Category: ${eventDetailed.category}</li>
+                            <li class="list-group-item">Price: $${eventDetailed.price}</li>
+                        </ul>
+                        <div class="card-body">
+                            <li class="list-group-item">Place: ${eventDetailed.place}</li>
+                            <li class="list-group-item">Capacity: ${eventDetailed.capacity}</li>
+                        </div>
+                    </div>`
+        
+        contenedor.innerHTML = tarjetas
+
+    }
+    catch (error) {
+        console.log("ERROOOOR");
+    }
+ }
+
+ getEvents()
